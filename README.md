1fc1148194e664a4c55915a9cd30357cea6d4355
Aspect1103 <jack.ashwell1@gmail.com>
2022-11-20 20:42:28 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+FIRE Assignment
+---------------
+
+This is for an assignment for a University module.
---
+++ Tester.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/**
+ * Write a description of class Tester here.
+ *
+ * @author
+ */
+public class Tester {
+    private void doTest() {
+        FIRE fortunate = new Resort("Fortunate Islands");
+        Scanner enter = new Scanner(System.in);
+    }
+
+    // No need to change this
+    public static void main(String[] args) {
+        Tester xx = new Tester();
+        xx.doTest();
+    }
+}
