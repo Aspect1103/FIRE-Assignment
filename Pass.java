f349c75e25d3018e961ed533a105d23a422eb3e3
Aspect1103 <jack.ashwell1@gmail.com>
2022-11-22 13:48:22 +0000

    Completed task 1.
    Updated .gitignore.

--- Pass.java
+++ Pass.java
@@ -6,5 +6,70 @@
  * @version (a version number or a date)
  */
 public class Pass {
-    private int passId;
+    private int passIdNumber;
+    private String guestName;
+    private int luxuryRating;
+    private int credits;
+    private int journeyPoints;
+
+    public Pass(int passId, String name, int luxury, int credit, int journey) {
+        passIdNumber = passId;
+        guestName = name;
+        luxuryRating = luxury;
+        credits = credit;
+        journeyPoints = journey;
+    }
+
+    public int getPassIdNumber() {
+        return passIdNumber;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public int getLuxuryRating() {
+        return luxuryRating;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public int getJourneyPoints() {
+        return journeyPoints;
+    }
+
+    public void addCredits(int points) {
+        credits += points;
+    }
+
+    public void removeCredits(int points) {
+        if (credits > 0) {
+            credits -= points;
+        }
+    }
+
+    public void useFerry() {
+        if (credits >= 3) {
+            credits -= 3;
+            journeyPoints += 1;
+        }
+    }
+
+    public void journeyPointsToCredits() {
+        if (journeyPoints >= 5) {
+            journeyPoints -= 5;
+            credits += 1;
+        }
+    }
+
+    public String toString() {
+        return "********************\nGuest Name: " +
+                guestName + "\nPass ID Number: " +
+                passIdNumber + "\nLuxury Rating: " +
+                luxuryRating + "\nCredits: " +
+                credits + "\nJourney Points: " +
+                journeyPoints + "\n********************";
+    }
 }
