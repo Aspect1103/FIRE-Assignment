1671e448c7478f1b557da0ea72ec5c512768e6b0
HeatOn <95578244+1Klevi1@users.noreply.github.com>
2022-11-23 20:55:33 +0000

    Completed task 2.1

--- BenchTesting.java
+++ BenchTesting.java
@@ -1,2 +1,83 @@
+import java.util.ArrayList;
+
+
 public class BenchTesting {
+    public static void main(String[] args) {
+
+        // Initialise some islands
+
+        Island island1= new Island(111,"Haven",5,2);
+        Island island2 = new Island(222,"Sweetfish",7,2 );
+        Island island3= new Island(333,"Seahorse",2,1 );
+        Island island4 = new Island(444,"Egg",10,2);
+
+        // Test the accessors
+        System.out.println("Expected: 111. Actual: "+ island1.islandNumber());
+        System.out.println("Expected: Sweetfish. Actual: "+ island2.islandName());
+        System.out.println("Expected: 2. Actual: "+ island3.islandRating());
+        System.out.println("Expected: 2. Actual: "+ island1.islandCapacity());
+
+        //Test the methods
+        Pass pass1 = new Pass(123, "Temp1", 5, 10, 1);
+        Pass pass2 = new Pass(456, "Temp2", 3, 20, 6);
+        Pass pass3 = new Pass(789, "Temp3", 8, 5, 0);
+        Pass pass4 = new Pass(111, "Temp4", 10, 2, 3);
+
+        island1.enter(pass1);
+        island2.enter(pass2);
+        island3.enter(pass3);
+        island3.enter(pass4);
+
+
+        island4.enter(pass1);
+        System.out.println("Expected : The island has capacity. Actual: "+island4.capacity());
+        island4.listCurrentPasses();
+        System.out.println();
+        island4.enter(pass2);
+        System.out.println("Expected : The island reached the maximum capacity. Actual: "+island4.capacity());
+        island4.listCurrentPasses();
+        System.out.println();
+        island4.enter(pass3);
+        System.out.println("Expected : The island reached the maximum capacity. Actual: "+island4.capacity());
+        island4.listCurrentPasses();
+        System.out.println();
+        island4.leave(pass2);
+        System.out.println("Expected : The island has capacity. Actual: "+island4.capacity());
+        island4.listCurrentPasses();
+        System.out.println();
+        island4.leave(pass1);
+        System.out.println("Expected : The island has capacity. Actual: "+island4.capacity());
+        island4.listCurrentPasses();
+
+//         Test the accessor
+        System.out.println("Expected: The island has capacity. Actual: "+ island1.capacity());
+        System.out.println("Expected: The island has capacity. Actual: "+island2.capacity());
+        System.out.println("Expected: The island reached the maximum capacity. Actual: "+island3.capacity());
+        island3.leave(pass4);
+        System.out.println("Expected: The island reached the maximum capacity. Actual: :"+island3.capacity());
+
+
+//         Test the methods
+        island1.listCurrentPasses();//Expected : temp1.
+        island2.listCurrentPasses();//Expected : temp2.
+        island3.listCurrentPasses();// Expected : temp3.
+
+
+        System.out.println(island1.toString());
+        System.out.println(island2.toString());
+        System.out.println(island3.toString());
+
+
+
+
+        System.out.println("Expected: temp1. Actual: "+island1.getPassDetails(123));
+
+        System.out.println("Expected: true. Actual: "+island1.isPassOnIsland(pass1));
+        System.out.println("Expected: false. Actual: "+island1.isPassOnIsland(pass2));
+
+
+    }
+
+
+
 }
--- Island.java
+++ Island.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 /**
  * An island is part of a FIRE resort.Each island has a name,  a luxury rating
  * and a capacity which represents the maximum number of people(passes) who can be on the
@@ -10,8 +11,63 @@ import java.util.*;
  * @author (20067052)
  * @version (a version number or a date)
  */
-
 public class Island {
+    private int island_ID;
+    private String island_Name;
+    private int island_Rating;
+    private int island_capacity;
+    private ArrayList<Pass>passList;
 
+    public Island(int id,String name,int rate,int capacity){
+        island_ID=id;
+        island_Name=name;
+        island_Rating=rate;
+        island_capacity=capacity;
+        passList=new ArrayList<Pass>(island_capacity);
+    }
+    public int islandNumber(){return island_ID;}
+    public String islandName(){return island_Name;}
+    public int islandRating(){return island_Rating;}
+    public int islandCapacity(){return island_capacity;}
 
+    public void enter(Pass temp){
+        if(passList.size()<island_capacity){
+            passList.add(temp);
+        }
+    }
+    public void leave(Pass temp){
+        passList.remove(temp);
+    }
+public String capacity() {
+        if (passList.size()<island_capacity)
+    {
+        return "The island has capacity";
+    }else return "The island reached the maximum capacity";
 }
+    public void listCurrentPasses(){
+
+        for(Pass p :passList){
+            System.out.println(p);
+        }}
+    public Pass getPassDetails(int id) {
+        for (Pass temp:passList){
+            if(temp.getPassIdNumber()==id){
+                return temp;
+            }
+        }
+        return null;
+    }
+
+    public boolean isPassOnIsland(Pass p){
+        return passList.contains(p);
+    }
+public String toString() {
+    return "********************\nIsland ID Number: " +
+            island_ID + "\nIsland Name: " +
+            island_Name + "\nIsland Rating: " +
+            island_Rating + "\nIsland capacity: " +
+            island_capacity + "\nPass List: "+
+            passList + "\n********************";
+}
+}
+
